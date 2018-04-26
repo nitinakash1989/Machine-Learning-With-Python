4e966289db3efd2bf5ff5d90d4f1eec3de991936
Nitin Akash <nitin.akash1989@gmail.com>
2018-04-25 21:54:29 -0300

    Handling missing values using Imputer

--- dataPreprocessExample.py
+++ dataPreprocessExample.py
@@ -14,3 +14,10 @@ X = dataset.iloc[:,:-1].values
 #Dependendent variable
 y = dataset.iloc[:,3].values
 
+#Handling missing values in dataset
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values='NaN', strategy= 'mean', axis= 0)
+
+#To fit imputer in matrix of feature X
+imputer = imputer.fit(X[:, 1:3])
+X[:,1:3] = imputer.transform(X[:,1:3])
