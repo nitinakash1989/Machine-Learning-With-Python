eb45c2fd1bc6ccd172f3c2cc973b3bd03ee5f3f5
Nitin Akash <nitin.akash1989@gmail.com>
2018-04-25 21:35:49 -0300

    Creating matrix of feature using iloc function.

--- dataPreprocessExample.py
+++ dataPreprocessExample.py
@@ -5,3 +5,11 @@ import pandas as pd
 
 #Importing dataset
 dataset = pd.read_csv("Data.csv")
+
+#Creating matrix of feature
+
+#Independent variable
+X = dataset.iloc[:,:-1]
+
+#Dependendent variable
+y = dataset.iloc[:,3]
