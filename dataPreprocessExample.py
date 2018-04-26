2f4b792e82a31e78699809314bb66f84c00fff58
Nitin Akash <nitin.akash1989@gmail.com>
2018-04-25 21:52:54 -0300

    Added values function to make it as an object from dataframe

--- dataPreprocessExample.py
+++ dataPreprocessExample.py
@@ -9,7 +9,8 @@ dataset = pd.read_csv("Data.csv")
 #Creating matrix of feature
 
 #Independent variable
-X = dataset.iloc[:,:-1]
+X = dataset.iloc[:,:-1].values
 
 #Dependendent variable
-y = dataset.iloc[:,3]
+y = dataset.iloc[:,3].values
+
