d77d3fd5537da70ef1a2f1ea87f8cfa96c19ce45
Nitin Akash <nitin.akash1989@gmail.com>
2018-04-27 14:24:44 -0300

    Encoding categorical variable using LabelEncoder and OneHotEncoder

--- dataPreprocessExample.py
+++ dataPreprocessExample.py
@@ -17,13 +17,17 @@ y = dataset.iloc[:,3].values
 #Handling missing values in dataset
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values='NaN', strategy= 'mean', axis= 0)
-
 #To fit imputer in matrix of feature X
 imputer = imputer.fit(X[:, 1:3])
 X[:,1:3] = imputer.transform(X[:,1:3])
 
-#Encoding categorical variable using LabelEncoder
-from sklearn.preprocessing import LabelEncoder
+#Encoding categorical variable using LabelEncoder and onehotencoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelEncoder_X = LabelEncoder()
 X[:,0] = labelEncoder_X.fit_transform(X[:,0])
+oneHotEncoder = OneHotEncoder(categorical_features = [0])
+X = oneHotEncoder.fit_transform(X).toarray()
+labelEncoder_y = LabelEncoder()
+y = labelEncoder_y.fit_transform(y)
+
 
