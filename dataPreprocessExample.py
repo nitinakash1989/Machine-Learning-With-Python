c8ff90b3774d7f90fcac5920c94d1c3feb07c109
Nitin Akash <nitin.akash1989@gmail.com>
2018-04-27 14:26:14 -0300

    Splitting dataset into test and training set using crossvalidation

--- dataPreprocessExample.py
+++ dataPreprocessExample.py
@@ -31,3 +31,10 @@ labelEncoder_y = LabelEncoder()
 y = labelEncoder_y.fit_transform(y)
 
 
+#Splitting dataset into training set and testing set using train_test_split
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0) 
+
+
+
+
